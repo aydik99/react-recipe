fc7e777cb0e31d87b3b471a2352dd281710715b0
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2019-09-09 21:46:39 +0300

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Home from './pages/Home';
+import Recipes from './pages/Recipes';
+import SingleRecipe from './pages/SingleRecipe';
+import Default from './pages/Default';
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello from our
+        <Home />
+        <Recipes />
+        <SingleRecipe />
+        <Default />
       </div>
     )
   }
