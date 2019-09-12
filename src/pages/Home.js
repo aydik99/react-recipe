ac8535d65376e2b50d42fe67fe06d61e043482c2
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2019-09-12 09:30:36 +0300

    Add default page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 import Header from '../components/Header'
 
 export default class Home extends Component {
     render() {
         return (
-            <Header />
+            <Header title="amazing recipes">
+                <Link to="recipes" className="text-uppercase btn btn-secondary 
+                btn-lg mt-3">
+                    Search recipes
+                </Link>
+            </Header>
         )
     }
 }
