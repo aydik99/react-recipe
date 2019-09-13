6c4485be85f3e64a020a2c64f95c67eccb503668
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2019-09-13 23:51:12 +0300

    finish recipe

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react'
+import { recipeData } from '../data/tempList';
 
 export default class SingleRecipe extends Component {
+    
+    constructor(props) {
+        super(props);
+        const id = this.props.match.params.id;
+        this.state = {
+            recipe: recipeData,
+            id,
+            loading:false
+        }
+    }
     render() {
+        const {
+            image_url,
+            publisher,
+            publisher_url,
+            source_url,
+            title,
+            ingriedients
+        } = this.state.recipe;
+
         return (
             <div>
                 <h4>SinglePage</h4>
